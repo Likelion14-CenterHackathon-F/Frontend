d2fb3dde13bd9dc29f3fc396bd4ec67a8088c794
YJEND <sudo.cyj@gmail.com>
2026-07-30 18:12:12 +0900

    chore: 기본 세팅

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        This is a simple example of a React component using Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
