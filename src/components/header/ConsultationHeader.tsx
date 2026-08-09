98218a41f08d9814b99fbee8eb69f89aba67e31c
kmj0973 <kmj0973@naver.com>
2026-08-09 20:20:08 +0900

    fix: 공통 헤더 컴포넌트 수정

--- src/components/header/ConsultationHeader.tsx
+++ src/components/header/ConsultationHeader.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 export default function ConsultationHeader({ title, onBack }: HeaderProps) {
   return (
-    <header className="relative flex h-16.5 items-center px-5">
+    <header className="relative flex h-16.5 items-center px-5 pt-[env(safe-area-inset-top)]">
       {onBack && (
         <button type="button" onClick={onBack}>
           <img src={previousIcon} alt="뒤로가기 버튼" />
