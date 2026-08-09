35af123e4125a42f824e86a351ca687bb6dfc38b
kmj0973 <kmj0973@naver.com>
2026-08-09 20:26:43 +0900

    feat: 상담 대기실 페이지 라우팅 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import HomePage from "@/pages/home";
+import ConsultationWaitingPage from "@/pages/consultation-waiting";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/consultations/:id/waiting",
+        element: <ConsultationWaitingPage />,
       },
     ],
   },
