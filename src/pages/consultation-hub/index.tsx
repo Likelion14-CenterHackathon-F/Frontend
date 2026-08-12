94a32eb78ebc92110f5c77f9a59ea09fd86d7e8f
kmj0973 <kmj0973@naver.com>
2026-08-12 16:21:27 +0900

    feat: 공통 footer 컴포넌트 추가 및 header 대소문자 수정

--- src/pages/consultation-hub/index.tsx
+++ src/pages/consultation-hub/index.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import ConsultationHeader from "@/components/header/ConsultationHeader";
+import ConsultationHeader from "@/components/Header/ConsultationHeader";
 import ConsultationTabs from "./components/ConsultationTabs";
-import Button from "@/components/Button/Button";
 import ConsultationGuide from "./components/ConsultationGuide";
 import ConsultationHistoryList from "./components/ConsultationHistoryList";
 import ConsultationReservationList from "./components/ConsultationReservationList";
 import { ongoingConsultations } from "@/constants/consultationMockData";
+import ConsultationFooter from "@/components/Footer/ConsultationFooter";
 
 type ConsultationTab = "history" | "ongoing";
 
@@ -37,15 +37,7 @@ function ConsultationHubPage() {
         </section>
       </main>
 
-      <footer className="fixed inset-x-0 bottom-0 z-10 mx-auto w-full max-w-[430px] bg-[#FCFCFC] px-5 pb-[calc(14px+env(safe-area-inset-bottom))] pt-[14px]">
-        <Button
-          disabled
-          fullWidth
-          className="h-[62px] rounded-[37px] bg-[#EDECF2] text-base font-medium tracking-[-0.4px] text-[#9795A0] disabled:cursor-not-allowed"
-        >
-          {t("footer.enter")}
-        </Button>
-      </footer>
+      <ConsultationFooter disabled>{t("footer.enter")}</ConsultationFooter>
     </div>
   );
 }
