512d4d5d14cf9068226ff76629e3b680c5ea46b0
kmj0973 <kmj0973@naver.com>
2026-08-14 04:10:37 +0900

    feat: stt 시작 api 추가

--- src/apis/consultation/consultation.api.ts
+++ src/apis/consultation/consultation.api.ts
@@ -2,6 +2,7 @@ import type {
   ApiResponse,
   JoinConsultationResponse,
   JoinConsultationRequest,
+  StartSttAgentResponse,
 } from "@/types/consultation.type";
 import axiosInstance from "../axiosInstance";
 
@@ -13,5 +14,12 @@ export const joinConsultation = async (
     ApiResponse<JoinConsultationResponse>
   >(`/consultation/${appointmentId}/join`, body);
 
+  return data.data;
+};
+
+export const startSttAgent = async (appointmentId: number) => {
+  const { data } = await axiosInstance.post<
+    ApiResponse<StartSttAgentResponse>
+  >(`/consultation/${appointmentId}/stt/start`);
   return data.data;
 };
